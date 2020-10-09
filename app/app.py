960f07de0b04c81960d0d0354b1462eb2cf777fe
ps0317ix <38696393+ps0317ix@users.noreply.github.com>
2020-10-09 18:00:23 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -73,4 +73,4 @@ def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
